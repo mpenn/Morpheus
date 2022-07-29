6c5202a0e4808e8873baedd65054b90f1fd3c85c
Eli Fajardo <efajardo@nvidia.com>
2022-07-29 19:09:25 +0000

    ae inference fix

--- morpheus/stages/inference/auto_encoder_inference_stage.py
+++ morpheus/stages/inference/auto_encoder_inference_stage.py
@@ -102,13 +102,13 @@ class _AutoEncoderInferenceWorker(InferenceWorker):
 
         explain_df = pd.DataFrame(np.empty((batch.count,3),dtype=object), columns=["num_col_max_loss", "bin_col_max_loss", "cat_col_max_loss"])
         if batch.model is not None:
-            mse_loss, bce_loss,cce_loss, rloss_scores = batch.model.get_anomaly_score(data)
-            num_names, cat_names, bin_names= batch.model.return_feature_names()
+            mse_loss, bce_loss, cce_loss, rloss_scores = batch.model.get_anomaly_score(data)
+            num_names, cat_names, bin_names = batch.model.return_feature_names()
             vi_df= batch.model.get_variable_importance(num_names, cat_names, bin_names, mse_loss, bce_loss, cce_loss, data)
             for col in vi_df.columns:
                 explain_df[col] = vi_df[col]
 
-            zscores = (rloss_scores - batch.train_loss_scores.mean())/batch.train_loss_scores.std()
+            zscores = (rloss_scores - batch.train_scores_mean)/batch.train_scores_std
             rloss_scores = rloss_scores.reshape((batch.count, 1))
             zscores = np.absolute(zscores)
             zscores = zscores.reshape((batch.count, 1))
